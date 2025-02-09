d276c0113598a7229a562b44c05408da0a07a332
Amos Kipruto <kiprutoamos44@gmail.com>
2025-02-10 00:49:53 +0300

    Added Products table/ model to display in the admin.

--- members/admin.py
+++ members/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Members
+from . models import Product
 
 # Register your models here.
 class MemberAdmin(admin.ModelAdmin):
   list_display = ('firstname', 'lastname', 'phone', 'joined_date')
 
 admin.site.register(Members, MemberAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ('name', 'price', 'stock', 'image_url')
+
+admin.site.register(Product, ProductAdmin)
